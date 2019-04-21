ba3112fa9492016a1661d3f9dc5a9016f90b4f2c
Brendan Berg <brendan_berg@aol.com>
2019-04-21 16:03:29 +0200

    Add AVR_Periph as submodule
    AVR_Periph was added as a submodule as it unites multiple AVR components
    into specific use case. A first peripheral class was written to handle
    motor operations.

--- Motor.cpp
+++ Motor.cpp
@@ -2,4 +2,57 @@
 // Created by Brendan Berg on 2019-04-20.
 //
 
+#include <stdlib.h>
+
 #include <periph/Motor.hpp>
+
+
+Motor::Motor (volatile uint8_t* directionPinAPort, uint8_t directionPinABit,
+              volatile uint8_t* directionPinBPort, uint8_t directionPinBBit,
+              volatile uint8_t* encoderPinAPin, uint8_t encoderPinABit,
+              volatile uint8_t* encoderPinBPin, uint8_t encoderPinBBit,
+              volatile uint8_t* velocityPinPort, uint8_t velocityPinBit,
+              Timer* timer, uint8_t timerOCR)
+    : m_DirectionPinA(directionPinAPort, directionPinABit),
+    m_DirectionPinB(directionPinBPort, directionPinBBit),
+    m_EncoderPinA(encoderPinAPin, encoderPinABit),
+    m_EncoderPinB(encoderPinBPin, encoderPinBBit),
+    m_VelocityPin(velocityPinPort, velocityPinBit, timer, timerOCR)
+{
+    m_DirectionPinA.Disable();
+    m_DirectionPinB.Disable();
+
+    m_EncoderSteps = 0;
+    m_LastEncoderPinAState = m_EncoderPinA.GetState();
+}
+
+
+void Motor::SetVelocity (int16_t velocity)
+{
+    bool forward = (velocity > 0);
+
+    m_DirectionPinA.Set(forward);
+    m_DirectionPinB.Set(!forward);
+
+    m_VelocityPin.SetValue(static_cast<uint8_t>(abs(velocity)));
+}
+
+
+void Motor::Update ()
+{
+    bool encoderPinAState = m_EncoderPinA.GetState();
+    bool encoderPinBState = m_EncoderPinB.GetState();
+
+    if (encoderPinAState != m_LastEncoderPinAState)
+    {
+        m_EncoderSteps += ((encoderPinAState == encoderPinBState) ? -1 : +1);
+    }
+
+    m_LastEncoderPinAState = encoderPinAState;
+}
+
+
+int64_t Motor::GetEncoderSteps ()
+{
+    return m_EncoderSteps;
+}
--- include/periph/Motor.hpp
+++ include/periph/Motor.hpp
@@ -5,10 +5,49 @@
 #ifndef AVR_PERIPH_MOTOR_HPP
 #define AVR_PERIPH_MOTOR_HPP
 
+#include <stdint.h>
+
+#include <mcu/io/AnalogOutputPin.hpp>
+#include <mcu/io/DigitalOutputPin.hpp>
+#include <mcu/io/DigitalInputPin.hpp>
+#include <mcu/timer/TimerFastPWM.hpp>
+
 
 class Motor
 {
-    
+public:
+
+    Motor (volatile uint8_t* directionPinAPort, uint8_t directionPinABit,
+           volatile uint8_t* directionPinBPort, uint8_t directionPinBBit,
+           volatile uint8_t* encoderPinAPin, uint8_t encoderPinABit,
+           volatile uint8_t* encoderPinBPin, uint8_t encoderPinBBit,
+           volatile uint8_t* velocityPinPort, uint8_t velocityPinBit,
+           Timer* timer, uint8_t timerOCR);
+
+    void SetVelocity (int16_t velocity);
+
+    void Update ();
+
+    int64_t GetEncoderSteps ();
+
+
+private:
+
+    DigitalOutputPin m_DirectionPinA;
+
+    DigitalOutputPin m_DirectionPinB;
+
+    DigitalInputPin m_EncoderPinA;
+
+    DigitalInputPin m_EncoderPinB;
+
+    AnalogOutputPin m_VelocityPin;
+
+    bool m_LastEncoderPinAState;
+
+    int64_t m_EncoderSteps;
+
+
 };
 
 
